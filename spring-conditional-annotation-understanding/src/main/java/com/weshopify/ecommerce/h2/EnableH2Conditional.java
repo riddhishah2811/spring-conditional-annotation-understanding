0c86a857df4045788852858c2fe9ac85daaeb4ae
RIDDHI SHAH <riddhishah2222@gmail.com>
2024-04-08 00:02:24 +0530

    Now added the Mysql and H2 dependency and comented simultaneously to check which class is loading

--- spring-conditional-annotation-understanding/src/main/java/com/weshopify/ecommerce/h2/EnableH2Conditional.java
+++ spring-conditional-annotation-understanding/src/main/java/com/weshopify/ecommerce/h2/EnableH2Conditional.java
@@ -8,9 +8,20 @@ public class EnableH2Conditional implements Condition {
 
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-		String dbType = System.getProperty("dbType");
-		System.out.println("DB Type is "+dbType);
-		return dbType!=null && dbType.contentEquals("H2");
+//		String dbType = System.getProperty("dbType");
+//		System.out.println("DB Type is "+dbType);
+//		return dbType!=null && dbType.contentEquals("H2");
+		boolean enableH2 = false;
+		try {
+			Class<?> h2Driver = Class.forName("org.h2.Driver");
+			if(h2Driver!=null) {
+				System.out.println("Driver evaluating by conditional is:\t"+h2Driver.getName());
+				enableH2 = true;
+			}
+		} catch (ClassNotFoundException e) {
+			enableH2 = false;
+		}
+		return enableH2;
 	}
 
 }
--- spring-conditional-annotation-understanding/src/main/java/com/weshopify/ecommerce/mysql/EnableMysqlConditional.java
+++ spring-conditional-annotation-understanding/src/main/java/com/weshopify/ecommerce/mysql/EnableMysqlConditional.java
@@ -8,9 +8,21 @@ public class EnableMysqlConditional implements Condition {
 
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-		String dbType = System.getProperty("dbType");
-		System.out.println("DB Type is "+dbType);
-		return dbType!=null && dbType.contentEquals("MYSQL");
+//		String dbType = System.getProperty("dbType");
+//		System.out.println("DB Type is "+dbType);
+//		return dbType!=null && dbType.contentEquals("MYSQL");
+		
+		boolean enableMYSQL = false;
+		try {
+			Class<?> MYSQLDriver = Class.forName("com.mysql.cj.jdbc.Driver");
+			if(MYSQLDriver!=null) {
+				System.out.println("Driver evaluating by conditional is:\t"+MYSQLDriver.getName());
+				enableMYSQL = true;
+			}
+		} catch (ClassNotFoundException e) {
+			enableMYSQL = false;
+		}
+		return enableMYSQL;
 	}
 
 }
